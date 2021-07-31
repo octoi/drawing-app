52a53e593028609033b5e5897a1108398ac0dfe4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-31 14:05:22 +0530

    listeners

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector('canvas');
 
 window.addEventListener('load', () => {
     const ctx = canvas.getContext('2d'); // setting context of canvas to 2d
 
-    resize();
+    resize(); // resize 
+
+    let painting = false; // variable that track painting
+
+    // listeners
+
+    function startPosition() {
+        painting = true;
+    }
+
+    function finishedPosition() {
+        painting = false;
+    }
+
+    canvas.addEventListener('mousemove', startPosition);
+    canvas.addEventListener('mouseup', finishedPosition);
 });
 
 window.addEventListener('resize', resize); // resize when screen size changes
